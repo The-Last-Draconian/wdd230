8b379549676a9a0310271a78a331a5f0c0ea5b94
The-Last-Draconian <tay20045@byui.edu>
2021-09-18 03:23:48 -0600

    Commit & Stage

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ let date = new Date();
 let year = date.getFullYear();
 document.getElementById("year").textContent = year;
 document.getElementById("lastUpdate").textContent = "Last Updated: " + document.lastModified;
+
+//this is copied from the Lab video. I don't quite understand it but I think I get the gist of how it works.
