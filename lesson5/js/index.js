49e908e0a6a8f4913334793eecb8d0905f065f28
The-Last-Draconian <tay20045@byui.edu>
2021-10-15 08:32:57 -0600

    Commit

--- lesson5/js/index.js
+++ lesson5/js/index.js
@@ -4,8 +4,18 @@ function toggleMenu() {
 
 
 let date = new Date();
-let day = date.getDay();
+let day = date.getDate();
     let monthName = ["Jan", "Feb", "Mar", "April", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     let month = monthName[date.getMonth()]; //got this code from the internet
 let year = date.getFullYear();
 document.getElementById("date").textContent = `${day} ${month}, ${year}`;
+
+
+
+let dayNumber = date.getDay();
+let element = document.getElementById("message");
+    if (dayNumber == 5) {
+        element.classList.add("showme");
+    } else {
+        element.classList.add("hideme")
+    }
