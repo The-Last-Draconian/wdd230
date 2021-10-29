283f7faecdbc2d8f584f7822e8f25462465085c2
The-Last-Draconian <tay20045@byui.edu>
2021-10-29 07:30:07 -0600

    Commit

--- lesson7/lazyload/lazyload.js
+++ lesson7/lazyload/lazyload.js
@@ -1,4 +1,4 @@
-let images = document.querySelectorAll("[data-src]");
+/*let images = document.querySelectorAll("img[data-src]");
 
 function preloadImage(img) {
     let src = img.getAttribute("data-src");
@@ -27,3 +27,47 @@ let imageObserver = new IntersectionObserver((entries, imageObserver) => {
 images.forEach(image => {
     imageObserver.observe(image);
 });
+
+Above is the coding I did. Below is the coding I copied from Leo Aquino's
+codepen he made to help me. I'm not entirely sure what it is that makes
+it work yet but it does!
+*/
+
+const images = document.querySelectorAll('img[data-src]');
+
+const imageOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 5px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    }
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+
+        items.forEach((item) => {
+
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+                gsap.from(item.target, { filter: "blur(5rem)", transform: "rotateX(180deg)" });
+
+            }
+        });
+    }, imageOptions);
+    images.forEach((img) => {
+        observer.observe(img);
+
+    });
+} else {
+    images.forEach((img) => {
+        loadImages(img);
+    });
+
+}
+
